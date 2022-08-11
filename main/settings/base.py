5277b16d346d50dc3f3573c47d398f7e9d4f6517
slmnkankal <slmnknkl@gmail.com>
2022-08-11 11:37:16 +0300

    custom serializer

--- main/settings/base.py
+++ main/settings/base.py
@@ -126,3 +126,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+REST_AUTH_SERIALIZERS = {
+    'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
+}
