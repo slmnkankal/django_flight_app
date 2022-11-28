474fa722762fe3546a8d12575edf6bdc8b6b5165
slmnkankal <slmnknkl@gmail.com>
2022-11-28 15:15:52 +0300

    '*', '127.0.0.1' are added to ALLOWED_HOSTS

--- main/settings/base.py
+++ main/settings/base.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['web-production-0f17.up.railway.app']
+ALLOWED_HOSTS = ['*', 'web-production-0f17.up.railway.app', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-0f17.up.railway.app']
 
